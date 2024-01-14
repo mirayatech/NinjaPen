32e706d4ddb2a46f99b007fb7c7d16c7ec4c5f5a
〽️iraya <mirayaabrodi@gmail.com>
2024-01-14 10:41:35 +0100

    feat: optimize rendering and state management in App component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import useLocalStorage from "../../hooks/useLocalStorage";
 import { CodeEditor } from "../CodeEditor/CodeEditor";
 import "./App.css";
@@ -6,27 +7,54 @@ export default function App() {
   const [html, setHtml] = useLocalStorage("html", "");
   const [css, setCss] = useLocalStorage("css", "");
   const [javascript, setJavascript] = useLocalStorage("javascript", "");
+  const [compiledContent, setCompiledContent] = useState("");
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setCompiledContent(`
+        <html>
+          <body>${html}</body>
+          <style>${css}</style>
+          <script>${javascript}</script>
+        </html>
+      `);
+    }, 250);
+
+    return () => clearTimeout(timeout);
+  }, [html, css, javascript]);
 
   return (
-    <div className="pane top-pane">
-      <CodeEditor
-        language="xml"
-        displayName="HTML"
-        value={html}
-        onChange={setHtml}
-      />
-      <CodeEditor
-        language="css"
-        displayName="CSS"
-        value={css}
-        onChange={setCss}
-      />
-      <CodeEditor
-        language="javascript"
-        displayName="JavaScript"
-        value={javascript}
-        onChange={setJavascript}
-      />
-    </div>
+    <>
+      <div className="pane top-pane">
+        <CodeEditor
+          language="xml"
+          displayName="HTML"
+          value={html}
+          onChange={setHtml}
+        />
+        <CodeEditor
+          language="css"
+          displayName="CSS"
+          value={css}
+          onChange={setCss}
+        />
+        <CodeEditor
+          language="javascript"
+          displayName="JavaScript"
+          value={javascript}
+          onChange={setJavascript}
+        />
+      </div>
+      <div className="pane">
+        <iframe
+          srcDoc={compiledContent}
+          title="output"
+          sandbox="allow-scripts"
+          frameBorder="0"
+          width="100%"
+          height="100%"
+        />
+      </div>
+    </>
   );
 }
